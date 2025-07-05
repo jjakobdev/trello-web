4fb18663693d203eff9f43363ba427aa299e9377
Bao Tran <tq.bao@tego.global>
2025-07-05 16:04:50 +0700

    Added light dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Button from '@mui/material/Button'
 import AutoAwesomeMosaicIcon from '@mui/icons-material/AutoAwesomeMosaic';
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
     <>
       <div>jjakob dev</div>
+      <Typography variant='body2' color="text.secondary">Test Typography</Typography>
       <Button variant="contained">Hello world</Button>
       <AutoAwesomeMosaicIcon />
     </>
