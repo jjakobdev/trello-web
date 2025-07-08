3d86683a57836adb4a5b492caf3ab177dbcd8a48
Bao Tran <tq.bao@tego.global>
2025-07-08 17:38:13 +0700

    Basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import Button from '@mui/material/Button'
-import AutoAwesomeMosaicIcon from '@mui/icons-material/AutoAwesomeMosaic'
-import Typography from '@mui/material/Typography'
-
-// import useMediaQuery from '@mui/material/useMediaQuery'
 import Select from '@mui/material/Select'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
@@ -15,6 +10,7 @@ import Box from '@mui/material/Box'
 import {
   useColorScheme
 } from '@mui/material/styles'
+import Container from '@mui/material/Container'
 
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
@@ -52,36 +48,37 @@ function ModeSelect() {
   );
 }
 
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
-  // console.log('prefersDarkMode ', prefersDarkMode)
-  // console.log('prefersLightMode ', prefersLightMode)
-
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light');
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  );
-}
-
 function App() {
   return (
-    <>
-      <ModeSelect />
-      <hr />
-      <ModeToggle />
-      <hr />
-      <div>jjakob dev</div>
-      <Typography variant='body2' color="text.secondary">Test Typography</Typography>
-      <Button variant="contained">Hello world</Button>
-      <AutoAwesomeMosaicIcon />
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh', backgroundColor: 'primary.main' }}>
+      <Box sx={{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <ModeSelect />
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board Bar
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.dark',
+        width: '100%',
+        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board Content
+      </Box>
+    </Container>
   )
 }
 
